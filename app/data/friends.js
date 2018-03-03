f21c1188eb1624d75d366b104be918a3569c0088
Nick Holmin <niholm@jedunn.com>
2018-03-03 08:23:39 -0600

    homework 11

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+
+	{
+		name: "Troy",
+		photo: "https://s-media-cache-ak0.pinimg.com/originals/b1/b9/24/b1b92479ea8397441043237c1da5c8d1.jpg"
+		scores: [
+			"5",
+			"1",
+			"2",
+			"4",
+			"1",
+			"3",
+			"5",
+			"3",
+			"2",
+			"3"
+		]
+	},
+	{
+		name: "Maximus",
+		photo: "https://images-na.ssl-images-amazon.com/images/G/01/digital/video/hero/Movies/Top250/B005DNO7MQ_gladiator_UXPA1._RI_SX940_.jpg"
+		scores: [
+			"4",
+			"3",
+			"1",
+			"3",
+			"5",
+			"1",
+			"2",
+			"5",
+			"1",
+			"1"
+		]
+
+	},
+	{
+		name: "Leonidas",
+		photo: "http://cdn.collider.com/wp-content/uploads/2016/03/300-gerard-butler.jpg"
+		scores: [
+			"2",
+			"1",
+			"4",
+			"5",
+			"2",
+			"1",
+			"5",
+			"5",
+			"2",
+			"3"
+		]
+	}
+];
+
+module.exports = friends;
